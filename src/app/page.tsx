7bbb5dcb15a58920f24176a5c4b1749fe8656939
code-by-deveshwar <deveshwarzard@gmail.com>
2025-09-13 16:06:06 +0530

    removed blog section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Hero from "@/components/Hero"
 // import SkillsSection from "@/components/SkillsSection"
-import BlogPreview from "@/components/BlogPreview"
 import ExperienceTimeline from "@/components/ExperienceTimeline"
 import FooterProfile from "@/components/FooterProfile"
 
@@ -8,7 +7,6 @@ export default function Home() {
   return (
     <main>
       <Hero />
-      <BlogPreview />
       <ExperienceTimeline />
       <FooterProfile />
     </main>
